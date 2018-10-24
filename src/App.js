9dbb2f2e654e56d3d8182a113e02385a52729223
bibek <bibeksinghthapa@gmail.com>
2018-10-24 12:11:09 -0400

    addeing data to resumejson

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import About from './Components/About';
 import Header from './Components/Header';
+import Resume from './Components/Resume';
 import logo from './logo.svg';
 import $ from 'jquery';
 import './App.css';
@@ -40,6 +41,7 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
 
       </div>
     );
