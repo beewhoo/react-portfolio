e08e90cc0f861394efdae8bdd5d71c6428a6dd89
bibek <bibeksinghthapa@gmail.com>
2018-10-24 13:01:22 -0400

    added work resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -14,7 +14,6 @@ class Resume extends Component {
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
-            <p>Highlights</p>
             <p>{work.description2}</p>
         </div>
       })
